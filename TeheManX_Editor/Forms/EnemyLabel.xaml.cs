f31f9602a85fa255d15074a02209f9fa96d4ba13
PogChampGuy <Kuumba.Lambie@gmail.com>
2025-11-12 22:01:44 -0500

    added  colors for specfic object memory regions.

--- TeheManX_Editor/Forms/EnemyLabel.xaml.cs
+++ TeheManX_Editor/Forms/EnemyLabel.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System.Windows.Controls;
+using System.Windows.Media;
 
 namespace TeheManX_Editor.Forms
 {
@@ -17,7 +18,30 @@ namespace TeheManX_Editor.Forms
         #region Methods
         public void AssignTypeBorder(byte type)
         {
-
+            switch (type)
+            {
+                case 0: //Item Objects
+                    this.border.BorderBrush = Brushes.Blue;
+                    break;
+                case 1: //Misc Objects
+                    this.border.BorderBrush = Brushes.Purple;
+                    break;
+                case 2: //Effect Objects
+                    this.border.BorderBrush = Brushes.HotPink;
+                    break;
+                case 3: //Main Objects
+                    this.border.BorderBrush = Brushes.Red;
+                    break;
+                case 4: //Weapon Objects
+                    this.border.BorderBrush = Brushes.Green;
+                    break;
+                case 5: //Shot Objects
+                    this.border.BorderBrush = Brushes.Orange;
+                    break;
+                default:
+                    this.border.BorderBrush = Brushes.Black;
+                    break;
+            }
         }
         #endregion Methods
     }
